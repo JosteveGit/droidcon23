9b5c0434fd92266560da00bd35ad969ad36d9a64
josteveadekanbi <adekanbijosteve@gmail.com>
2023-10-21 19:33:45 +0100

    Clean up

--- lib/astro/stacky/astro.stack.dart
+++ lib/astro/stacky/astro.stack.dart
@@ -22,6 +22,10 @@ class _AstroStackPageState extends State<AstroStackPage>
   bool delayedInsideAstronaut = false;
   bool draggedMoonDown = false;
   bool delayedDagMoonDown = false;
+  bool onReadMore = false;
+
+  late final TabController tabController =
+      TabController(length: 3, vsync: this);
 
   double get target {
     return insideAstronaut ? 1 : 0;
@@ -36,7 +40,7 @@ class _AstroStackPageState extends State<AstroStackPage>
   }
 
   void onDraggedDown() async {
-    if (!draggedMoonDown) {
+    if (!draggedMoonDown && !onReadMore) {
       setState(() {
         draggedMoonDown = true;
       });
@@ -48,7 +52,7 @@ class _AstroStackPageState extends State<AstroStackPage>
   }
 
   void onDraggedUp() async {
-    if (draggedMoonDown) {
+    if (draggedMoonDown && !onReadMore) {
       setState(() {
         delayedDagMoonDown = false;
       });
@@ -299,31 +303,38 @@ class _AstroStackPageState extends State<AstroStackPage>
 
                     //MOON.
                     AnimatedPositioned(
-                      top: delayedDagMoonDown
-                          ? 650
-                          : insideAstronaut
-                              ? 320
-                              : 200,
+                      top: onReadMore
+                          ? -70
+                          : delayedDagMoonDown
+                              ? 650
+                              : insideAstronaut
+                                  ? 320
+                                  : 200,
                       left: 0,
                       right: 0,
                       duration: const Duration(milliseconds: 1000),
                       curve: Curves.easeInOut,
                       child: AnimatedScale(
-                        scale: delayedDagMoonDown ? 2.5 : 1,
+                        scale: onReadMore ? 0.6 : 1,
                         duration: const Duration(milliseconds: 1000),
                         curve: Curves.easeInOut,
-                        child: AnimatedOpacity(
-                          opacity: insideAstronaut ? 1 : 0,
-                          duration: const Duration(milliseconds: 500),
+                        child: AnimatedScale(
+                          scale: delayedDagMoonDown ? 2.5 : 1,
+                          duration: const Duration(milliseconds: 1000),
                           curve: Curves.easeInOut,
-                          child: AnimatedScale(
-                            scale: insideAstronaut ? 1.5 : 0,
-                            duration: const Duration(milliseconds: 1000),
+                          child: AnimatedOpacity(
+                            opacity: insideAstronaut ? 1 : 0,
+                            duration: const Duration(milliseconds: 500),
                             curve: Curves.easeInOut,
-                            child: Center(
-                              child: Image.asset(
-                                "assets/images/moon.png",
-                                width: 130,
+                            child: AnimatedScale(
+                              scale: insideAstronaut ? 1.5 : 0,
+                              duration: const Duration(milliseconds: 1000),
+                              curve: Curves.easeInOut,
+                              child: Center(
+                                child: Image.asset(
+                                  "assets/images/moon.png",
+                                  width: 130,
+                                ),
                               ),
                             ),
                           ),
@@ -331,11 +342,13 @@ class _AstroStackPageState extends State<AstroStackPage>
                       ),
                     ),
                     AnimatedPositioned(
-                      top: delayedDagMoonDown
-                          ? 550
-                          : insideAstronaut
-                              ? 250
-                              : 135,
+                      top: onReadMore
+                          ? -220
+                          : delayedDagMoonDown
+                              ? 550
+                              : insideAstronaut
+                                  ? 250
+                                  : 135,
                       left: 0,
                       right: 0,
                       duration: const Duration(milliseconds: 1000),
@@ -359,11 +372,13 @@ class _AstroStackPageState extends State<AstroStackPage>
                       ),
                     ),
                     AnimatedPositioned(
-                      top: delayedDagMoonDown
-                          ? 580
-                          : insideAstronaut
-                              ? 270
-                              : 240,
+                      top: onReadMore
+                          ? -100
+                          : delayedDagMoonDown
+                              ? 580
+                              : insideAstronaut
+                                  ? 270
+                                  : 240,
                       left: 0,
                       right: 0,
                       duration: const Duration(milliseconds: 1000),
@@ -377,17 +392,22 @@ class _AstroStackPageState extends State<AstroStackPage>
                           duration: const Duration(milliseconds: 1000),
                           curve: Curves.easeInOut,
                           child: Center(
-                            child: AnimatedDefaultTextStyle(
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: delayedDagMoonDown
-                                    ? Colors.white
-                                    : Colors.white.withOpacity(0.3),
-                                fontWeight: FontWeight.bold,
-                              ),
-                              duration: const Duration(milliseconds: 400),
-                              child: const Text(
-                                "MOON",
+                            child: GestureDetector(
+                              onTap: () {
+                                print("happy pres");
+                              },
+                              child: AnimatedDefaultTextStyle(
+                                style: TextStyle(
+                                  fontSize: onReadMore ? 16 : 12,
+                                  color: delayedDagMoonDown
+                                      ? Colors.white
+                                      : Colors.white.withOpacity(0.3),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                duration: const Duration(milliseconds: 400),
+                                child: const Text(
+                                  "MOON",
+                                ),
                               ),
                             ),
                           ),
@@ -421,35 +441,193 @@ class _AstroStackPageState extends State<AstroStackPage>
                         ),
                       ),
                     ),
+                    AnimatedPositioned(
+                      bottom: delayedDagMoonDown ? 25 : -100,
+                      left: 0,
+                      right: 0,
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.easeInOut,
+                      child: AnimatedOpacity(
+                        opacity: onReadMore ? 0 : 1,
+                        duration: const Duration(milliseconds: 1000),
+                        child: AnimatedOpacity(
+                          opacity: delayedDagMoonDown ? 1 : 0,
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                          child: Center(
+                            child: Column(
+                              children: [
+                                const Text(
+                                  "Is an astronomical body that orbits planet Earth, being\nEarth's only permanent natural satellite...",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 8,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      onReadMore = true;
+                                    });
+                                  },
+                                  child: const Text(
+                                    "Read more",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 8,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // INDICATOR.
                     Positioned(
                       left: 0,
                       right: 0,
                       bottom: 200,
                       child: AnimatedOpacity(
-                        opacity: delayedInsideAstronaut ? 1 : 0,
+                        opacity: onReadMore ? 0 : 1,
                         duration: const Duration(milliseconds: 900),
                         curve: Curves.easeInOut,
-                        child: Center(
-                          child: Column(
-                            children: [
-                              for (int i = 0; i < 2; i++) ...[
-                                AnimatedContainer(
-                                  width: i != viewingIndex ? 8 : 6,
-                                  height: i != viewingIndex ? 8 : 6,
-                                  duration: const Duration(milliseconds: 400),
-                                  curve: Curves.easeInOut,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: i != viewingIndex
-                                        ? Colors.white
-                                        : Colors.white.withOpacity(0.3),
+                        child: AnimatedOpacity(
+                          opacity: delayedInsideAstronaut ? 1 : 0,
+                          duration: const Duration(milliseconds: 900),
+                          curve: Curves.easeInOut,
+                          child: Center(
+                            child: Column(
+                              children: [
+                                for (int i = 0; i < 2; i++) ...[
+                                  AnimatedContainer(
+                                    width: i != viewingIndex ? 8 : 6,
+                                    height: i != viewingIndex ? 8 : 6,
+                                    duration: const Duration(milliseconds: 400),
+                                    curve: Curves.easeInOut,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: i != viewingIndex
+                                          ? Colors.white
+                                          : Colors.white.withOpacity(0.3),
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(height: 5),
+                                  const SizedBox(height: 5),
+                                ],
                               ],
-                            ],
+                            ),
                           ),
                         ),
+                      ),
+                    ),
+
+                    AnimatedPositioned(
+                      top: onReadMore ? -40 : 1000,
+                      left: 0,
+                      right: 0,
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.easeInOut,
+                      child: Container(
+                        color: Colors.white,
+                        height: MediaQuery.of(context).size.height,
+                        width: double.maxFinite,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              controller: tabController,
+                              indicatorColor: Colors.black,
+                              labelColor: Colors.black,
+                              tabs: const [
+                                Tab(text: "DISCOVER"),
+                                Tab(text: "HISTORY"),
+                                Tab(text: "DIAGRAMS"),
+                              ],
+                            ),
+                            Expanded(
+                              child: TabBarView(
+                                controller: tabController,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(20),
+                                    child: ListView(
+                                      children: [
+                                        const SizedBox(height: 20),
+                                        const Text(
+                                          "The Moon",
+                                          style: TextStyle(
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 30),
+                                        const Text(
+                                          "Is an astronomical body that orbits planet Earth, being Earth's only permanent natural satellite. It is the fifth-largest natural satellite in the Solar System, and the largest among planetary satellites relative to the size of the planet that it orbits (its primary). Following Jupiter's satellite Io, the Moon is second-densest satellite among those whose densities are known.",
+                                          style: TextStyle(fontSize: 14),
+                                        ),
+                                        const SizedBox(height: 20),
+                                        SizedBox(
+                                          height: 150,
+                                          width: double.maxFinite,
+                                          child: ListView(
+                                            scrollDirection: Axis.horizontal,
+                                            children: [
+                                              Container(
+                                                color: Colors.black,
+                                                width: 300,
+                                              ),
+                                              const SizedBox(width: 20),
+                                              Container(
+                                                color: Colors.grey,
+                                                width: 300,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        const SizedBox(height: 20),
+                                        const Text(
+                                          "Formation",
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 30),
+                                        const Text(
+                                          "The prevailing hypothesis is that the Earth–Moon system formed after an impact of a Mars-sized body (named Theia) with the proto-Earth more than 4.5 billion years ago (Ga). The impact blasted material into Earth's orbit and the material accreted and formed the Moon. The Moon's far side has a crust that is 30 mi (48 km) thicker than that of the near side. This is thought to be due to the Moon having been amalgamated from two different bodies. Some astronomers think that about 4.5 Ga, an object the size of Mars, sometimes called Theia, collided with the ancient Earth (called Gaia) and splashed billions of tons of debris into orbit around it. This debris settled around Gaia in an orbiting mass, creating a proto-lunar disk. Eventually, material in this disk began to coalesce, forming the Moon.",
+                                          style: TextStyle(fontSize: 14),
+                                        ),
+                                        const SizedBox(height: 200),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(),
+                                  Container(),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    AnimatedPositioned(
+                      top: 20,
+                      left: onReadMore ? 20 : -50,
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.easeInOut,
+                      child: GestureDetector(
+                        onTap: () {
+                          print("hi");
+                          setState(() {
+                            onReadMore = false;
+                          });
+                        },
+                        child: const Text("< Back"),
                       ),
                     ),
                   ],
